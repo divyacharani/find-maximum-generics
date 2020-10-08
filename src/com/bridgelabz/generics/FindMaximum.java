d6b25d71f8e13d3f8e0b38ef9a23a73c01be147f
unknown <divyacharaninabha@gmail.com>
2020-10-09 00:20:46 +0530

    Added: Welcome message added

---
+++ src/com/bridgelabz/generics/FindMaximum.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.generics;
+
+public class FindMaximum {
+
+	public static void main(String[] args) {
+		//Welcome Message
+		System.out.println("Welcome to Find Maximum Program using Generics");
+	}
+}
