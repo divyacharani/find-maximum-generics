77cf4f32d219101d4114df544bda96f2d97d2ea7
unknown <divyacharaninabha@gmail.com>
2020-10-09 20:13:45 +0530

    Modified: Functionality to modified to find maximum among more than three values

--- src/com/bridgelabz/generics/FindMaximum.java
+++ src/com/bridgelabz/generics/FindMaximum.java
@@ -1,68 +1,31 @@
 package com.bridgelabz.generics;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class FindMaximum <E extends Comparable<E>>{
-	
-	private E first, second, third;
+public class FindMaximum<E> {
 
-	//Construcor
-	public FindMaximum(E first, E second, E third) {
-		super();
-		this.first = first;
-		this.second = second;
-		this.third = third;
-	}
-	
-	// Generic method to find maximum value
-	public  E findMaximum() {
-		E max = first;
-		if (second.compareTo(max) > 0)
-			max = second;
-		if (third.compareTo(max) > 0)
-			max = third;
+	// Generic method to find maximum value for variable number of arguments
+	public E findMaximum(E... values) {
+		Arrays.sort(values);
+		E max = values[values.length - 1];
 		return max;
 	}
 
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		// Welcome Message
 		System.out.println("Welcome to Find Maximum Program using Generics");
 
-		// Input for integers
-		System.out.println("Enter first integer number");
-		int firstIntNum = sc.nextInt();
-		System.out.println("Enter second integer number");
-		int secondIntNum = sc.nextInt();
-		System.out.println("Enter third integer number");
-		int thirdIntNum = sc.nextInt();
-		FindMaximum<Integer> integerObj = new FindMaximum<Integer>(firstIntNum, secondIntNum, thirdIntNum);
+		FindMaximum<Integer> integerObj = new FindMaximum<Integer>();
+		FindMaximum<Double> doubleObj = new FindMaximum<Double>();
+		FindMaximum<String> stringObj = new FindMaximum<String>();
 
-		// Input for float values
-		System.out.println("Enter first float number");
-		double firstFloatNum = sc.nextDouble();
-		System.out.println("Enter second float number");
-		double secondFloatNum = sc.nextDouble();
-		System.out.println("Enter third float number");
-		double thirdFloatNum = sc.nextDouble();
-		FindMaximum<Double> doubleObj = new FindMaximum<Double>(firstFloatNum, secondFloatNum, thirdFloatNum);
-
-		// Input for strings
-		System.out.println("Enter first string");
-		sc.nextLine();
-		String firstString = sc.nextLine();
-		System.out.println("Enter second string");
-		String secondString = sc.nextLine();
-		System.out.println("Enter third string");
-		String thirdString = sc.nextLine();
-		FindMaximum<String> stringObj = new FindMaximum<String>(firstString, secondString, thirdString);
-		
-		System.out.println("The maximum number among entered integers is "
-				+ integerObj.findMaximum());
+		System.out.println("The maximum number among entered integers is " + integerObj.findMaximum(2, 5, 1, -8));
 		System.out.println("The maximum number among entered float numbers is "
-				+ doubleObj.findMaximum());
-		System.out.println("The highest string is  " + stringObj.findMaximum());
+				+ doubleObj.findMaximum(6.7, 4.89, -9.87, 5.67, 10.24));
+		System.out.println("The highest string is  " + stringObj.findMaximum("Apple", "Banana"));
 		sc.close();
 	}
 }
