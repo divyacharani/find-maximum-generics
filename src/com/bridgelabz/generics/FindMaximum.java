e301b71c75f81af38969780837dee294225b68d5
unknown <divyacharaninabha@gmail.com>
2020-10-09 10:54:28 +0530

    Added: Functionality to find maximum among float numbers added

--- src/com/bridgelabz/generics/FindMaximum.java
+++ src/com/bridgelabz/generics/FindMaximum.java
@@ -4,9 +4,19 @@ import java.util.Scanner;
 
 public class FindMaximum {
 
-	// Method to find maximum value
-	public static Integer findMaximum(Integer firstNum, Integer secondNum, Integer thirdNum) {
+	// Method to find maximum value among integers
+	public static Integer findMaximumInteger(Integer firstNum, Integer secondNum, Integer thirdNum) {
 		Integer max = firstNum;
+		if (secondNum.compareTo(max) > 0)
+			max = secondNum;
+		if (thirdNum.compareTo(max) > 0)
+			max = thirdNum;
+		return max;
+	}
+
+	// Method to find maximum value among float values
+	public static Double findMaximumFloat(Double firstNum, Double secondNum, Double thirdNum) {
+		Double max = firstNum;
 		if (secondNum.compareTo(max) > 0)
 			max = secondNum;
 		if (thirdNum.compareTo(max) > 0)
@@ -18,13 +28,26 @@ public class FindMaximum {
 		Scanner sc = new Scanner(System.in);
 		// Welcome Message
 		System.out.println("Welcome to Find Maximum Program using Generics");
-		System.out.println("Enter first number");
-		int firstNum = sc.nextInt();
-		System.out.println("Enter second number");
-		int secondNum = sc.nextInt();
-		System.out.println("Enter third number");
-		int thirdNum = sc.nextInt();
-		System.out.println("The maximum number is " + findMaximum(firstNum, secondNum, thirdNum));
+
+		// Input for integers
+		System.out.println("Enter first integer number");
+		int firstIntNum = sc.nextInt();
+		System.out.println("Enter second integer number");
+		int secondIntNum = sc.nextInt();
+		System.out.println("Enter third integer number");
+		int thirdIntNum = sc.nextInt();
+
+		// Input for float values
+		System.out.println("Enter first float number");
+		double firstFloatNum = sc.nextDouble();
+		System.out.println("Enter second float number");
+		double secondFloatNum = sc.nextDouble();
+		System.out.println("Enter third float number");
+		double thirdFloatNum = sc.nextDouble();
+		System.out.println("The maximum number among entered integers is "
+				+ findMaximumInteger(firstIntNum, secondIntNum, thirdIntNum));
+		System.out.println("The maximum number among entered float numbers is "
+				+ findMaximumFloat(firstFloatNum, secondFloatNum, thirdFloatNum));
 		sc.close();
 	}
 }
--- test/com/bridgelabz/generics/TestFindMaximum.java
+++ test/com/bridgelabz/generics/TestFindMaximum.java
@@ -9,19 +9,36 @@ public class TestFindMaximum {
 	@Test
 	public void testMaxIntFirstPosition() {
 		Integer expected = 10;
-		assertEquals(expected, FindMaximum.findMaximum(10, 1, 4));	//Integer at first position
+		assertEquals(expected, FindMaximum.findMaximumInteger(10, 1, -4)); // Integer at first position
 	}
-	
+
 	@Test
 	public void testMaxIntSecondPosition() {
 		Integer expected = 10;
-		assertEquals(expected, FindMaximum.findMaximum(1, 10, 4));	//Integer at second position
+		assertEquals(expected, FindMaximum.findMaximumInteger(1, 10, -4)); // Integer at second position
 	}
-	
+
 	@Test
 	public void testMaxIntThirdPosition() {
 		Integer expected = 10;
-		assertEquals(expected, FindMaximum.findMaximum(4, 1, 10));	//Integer at third position
+		assertEquals(expected, FindMaximum.findMaximumInteger(-4, 1, 10)); // Integer at third position
 	}
 
+	@Test
+	public void testMaxFloatFirstPosition() {
+		Double expected = 9.78;
+		assertEquals(expected, FindMaximum.findMaximumFloat(9.78, -5.6, 2.35)); // Float at first position
+	}
+
+	@Test
+	public void testMaxFloatSecondPosition() {
+		Double expected = 9.78;
+		assertEquals(expected, FindMaximum.findMaximumFloat(-5.6, 9.78, 2.35)); // Float at first position
+	}
+
+	@Test
+	public void testMaxFloatThirdPosition() {
+		Double expected = 9.78;
+		assertEquals(expected, FindMaximum.findMaximumFloat(-5.6, 2.35, 9.78)); // Float at first position
+	}
 }
